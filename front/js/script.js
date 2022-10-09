c7a84042e94f722d9457328887a3b0a5d278afef
tenma1005 <sammy.andrade@outlook.fr>
2022-10-09 15:28:55 +0200

    script final

--- front/js/script.js
+++ front/js/script.js
@@ -2,22 +2,37 @@
 SCRIPT DE LA PAGE D'ACCUEIL -> LE BUT EST DE FAIRE AFFICHER TOUS LES PRODUITS EN BASE DE DONNÉE
 ===============================================================================================*/
 
+/*--------------------------------------------------------------------------
+// PARTIE 1 : Appel de l'API de façon asynchrone
+----------------------------------------------------------------------------*/
+
 /* Pour commencer, on fait une fonction async qu'on appelle dataBase() pour récupérer l'URL de la base de 
    donnée et pour faire une promise (.then + .catch) en utilisant la méthode fetch */
 
 /* Après ça, dans la fonction dataBase() On demande la réponse de l'API en format JSON en fesant un return */
 async function dataBase() {
   return fetch("http://localhost:3000/api/products")
-    .then((apiResponse) => /*c'est un return */ apiResponse.json())
-    .then((dataTable) => /*console.table (on fait un return...)*/ dataTable)
-    .catch((error) => {
-      document.querySelector(".titles").innerHTML = "<h1>error</h1>";
+    .then(function (apiResponse) {
+      return apiResponse.json();
+    })
+    .then(function (articlesList) {
+      console.table(articlesList);
+
+      return articlesList;
+    })
+    .catch(function (error) {
+      document.querySelector(".titles").innerHTML = "<h1>Error</h1>";
       console.log(
-        "Error, ça ne marche pas... À voir au niveau du fichier script.js"
+        "Error, ça ne marche pas... À voir au niveau du script ou de l'API" +
+          error
       );
-      /* si ça ne fonctionne pas, on affiche un message dans la console et on change le titre h1 et on retire le titre h2... */
+      /* si ça ne fonctionne pas, on affiche un message dans la console et on change le titre h1... */
     });
 }
+
+/*--------------------------------------------------------------------------
+// PARTIE 2 : Injection des resultats dans le html
+----------------------------------------------------------------------------*/
 
 /* Ensuite, on fait ensuite une fonction async que l'on nomme cardsKanap(), 
      on récupère après la réponse de la fonction dataBase() que l'on stocke dans une const qu'on nomme cards 
@@ -28,16 +43,18 @@ async function dataBase() {
      les produits dans la page d'acceuil en rejoutant par la suite document.querySelector("#items").innerHTML  */
 
 async function cardsKanap() {
-  const cards = await dataBase();
-  for (let card of cards) {
+  const articles = await dataBase();
+  for (let article of articles) {
     let i = 0;
-    i < card.length;
+    i < article.length;
     i++;
-    document.querySelector("#items").innerHTML += `<a href="product.html?id=${card._id}">
+    document.querySelector(
+      "#items"
+    ).innerHTML += `<a href="product.html?id=${article._id}">
                     <article>
-                      <img src="${card.imageUrl}" alt="${card.altTxt}">
-                      <h3 class="cardName">${card.name}</h3>
-                      <p class="cardDescription">${card.description}</p>
+                      <img src="${article.imageUrl}" alt="${article.altTxt}">
+                      <h3 class="cardName">${article.name}</h3>
+                      <p class="cardDescription">${article.description}</p>
                     </article>
                   </a>`;
   }
